6308cc97fd4dc6c4041a557975f28bb07119ba0d
willroberttv <willtaavares@gmail.com>
2020-07-29 19:33:37 -0300

    desenvolvimento da aplicação inicializada

---
+++ src/pages/Home/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+function Home() {
+  return <div />;
+}
+
+export default Home;
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function Routes() {
+  return <div />;
+}
+
+export default Routes;
