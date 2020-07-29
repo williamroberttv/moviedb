5d29c4728227f4557a2c23f763ba829b40cc40d5
willroberttv <willtaavares@gmail.com>
2020-07-29 19:51:11 -0300

    rotas adicionadas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-// import { Container } from './styles';
+import Home from './pages/Home';
+import Details from './pages/Details';
 
 function Routes() {
-  return <div />;
+  <BrowserRouter>
+    <Route exact path="/" component={Home} />
+    <Route path="/filme" component={Details} />
+  </BrowserRouter>;
 }
 
 export default Routes;
